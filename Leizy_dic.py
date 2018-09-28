7ea4e533dfb675ed1164f7f5ba10ab64664493f7
LeizyLee <36654052+LeizyLee@users.noreply.github.com>
2018-09-28 10:55:25 +0900

    Update Leizy_dic.py

--- Leizy_dic.py
+++ Leizy_dic.py
@@ -1,34 +1,56 @@
 # -*- coding : utf-8 -*-
 import collections
+"""
+__name__ : "Leizy_Lee"
+__date__ : "2018-09-04"
+__Email__ : "hwansang5@gmail.com"
+__Project__ : "Unmmaned Kiosk"
+"""
+# 포함도 -> 버튼이벤트 > UI > 내부 매커니즘
+"""
 
-class LeDict:
+def Rasp.오른쪽버튼:
+    버튼 동작시 오른쪽 버튼UI (클릭 이벤트) 발생
+
+def UI.오른쪽버튼클릭:
+    리스트에서 항목 하나를 오른쪽으로 이동!
+
+
+메인
+Rasp.오른쪽()
+
+결과
+(클릭 전)현재 항목 - 커피
+(Rasp.오른쪽() 실행 후) 현재 항목 - 맥주
+"""
+class unManBeta:
     def __init__(self):
-        self.menu_list = collections.OrderedDict()
-        self.PointList = []
-        print("!")
+        self.menulist = collections.OrderedDict()
+   
+    def addKey(self, _key):
+        if _key in list(self.menulist.keys()):
+            print("이미 있는 키\n")
+        else:
+            self.menulist[_key] = []
 
-    def is_int(self, _str="", _name=""):
-        flag = True
-        result = ""
+    def modifyKey(self, _key, _modify):
+        self.menulist[_modify] = self.menulist.pop(_key)
 
-        while flag:
-            if not _name == "":
-                result = input("%s %s"%(_name, _str))
-            else:
-                result = input(_str)
-            try:
-                int(result)
-            except ValueError:
-                print("오로지 숫자만 입력하시오.")
-            else:
-                flag = False
+    def delKey(self, _key):
+        del self.menulist[_key]
 
-        return result
+    def addKeyItem(self, _key, _item):
+        if _item in list(self.menulist.get(_key)):
+            print("이미 존재하는 값")
+        else:
+            self.menulist[_key].append(_item)
 
     def set_Dic(self):
         # 지역변수 선언
         file = open('menulist.txt', 'r')
+        #file = codecs.open('menulist.txt', 'r', 'UTF8')
         a = file.readline().strip()
+
         #a = self.is_int("카테고리의 갯수를 입력해주세요. : ")
         for i in range(0,int(a)):
             #s = input("%d번째 카테고리의 이름을 정해주세요. : " %(i+1))
@@ -45,13 +67,16 @@ class LeDict:
                 #item = input("%d 번째의 아이템 이름을 정해주세요. :"%(j+1))
                 tmp_list.append(item)
 
-            self.menu_list[s] = tmp_list
+            self.menulist[s] = tmp_list
             del(tmp_list)
 
-    def show_Dic(self):
+    def __del__(self):
+        print("종료\n")
+
+    def show(self):
         print("\n\n-!-한글로 카테고리를 작성했을 시, 테이블의 형태가 비정살일 수도 있음 -!-\n\n")
-        keys_list = list(self.menu_list.keys())
-        items_list = list(self.menu_list.values())
+        keys_list = list(self.menulist.keys())
+        items_list = list(self.menulist.values())
 
         print("---------------------------------------------------------------------------------------")
         print("|   카테고리   |                              아 이 템    항 목                          |")
@@ -60,93 +85,31 @@ class LeDict:
             print("|"+str(keys_list[i]).ljust(14) + "|" + str(items_list[i]).rjust(70) + "|")
         print("---------------------------------------------------------------------------------------")
 
-    def change_Dic_KeyName(self, _before="", _after = ""):
-        self.menu_list[_after] = self.menu_list.pop(_before)
+    def save_data(self):
+        key_list = list(self.menulist.keys())
+        items_list = list(self.menulist.values())
+        print(key_list, items_list)
+        print(items_list[0][0])
+        f = open("menulist.txt",'w')
+        data = str(len(key_list)) + "\n"
+        f.write(data)
+        for i in range(0,len(key_list)):
+            data = str(key_list[i]) + "\n"
+            f.write(data)
+            data = str(len(items_list[i])) + "\n"
+            f.write(data)
+            for j in range(0, len(items_list[i])):
+                data = str(items_list[i][j]) + "\n"    
+                f.write(data)
+    def get_key(self):
+        return self.menulist.keys()
 
-    def is_exist_Key(self, _key=""):
-        if _key in list(self.menu_list.keys()):
-            return False
-        else:
-            return True
+    def get_item(self, _key):
+        _tmp = list(self.menulist.keys())
+        _tmp_item = list(self.menulist.values())
+        for i in range(0,len(_tmp)):
+            if _key in _tmp[i]:
+                return _tmp_item[i]
 
-    def is_exist_Value(self, _Key="", _Value=""):
-        if _Value in list(self.menu_list.get(_Key)):
-            return False
-        else:
-            return True
 
-    def add_key(self, _key):
-        if self.is_exist_Key(_key):
-            self.menu_list[_key] = ""
-        else:
-            print("이미 존재하는 키")
 
-    def add_value(self, _key, _value):
-        if self.is_exist_Value(_key,_value):
-            self.menu_list[_key] = _value
-        else:
-            print("이미 해당 값이 들어있음")
-
-    def Do_Run(self):
-        tnum = 0
-        tlist = []
-        self.PointList = list(self.menu_list.keys())
-        tmp = ''
-
-        while not tmp == 'E':
-            print("시작을 원하시면 R을 눌러주세요!")
-            tmp = input()
-
-            tmp = input("카테고리를 선택해주세요. 첫 항목은 %s입니다. \n그리고 선택을 희망하시면 E를 입력해주세요.\n->"%self.PointList[tnum])
-            while not tmp == 'E':
-                if tmp == 'a':
-                    if tnum == 0:
-                        tnum = len(self.PointList) - 1
-                        tmp = input("현재 항목은 %s입니다.\n->"%self.PointList[tnum])
-                    else:
-                        tnum = tnum - 1
-                        tmp = input("현재 항목은 %s입니다.\n->" % self.PointList[tnum])
-
-                elif tmp == 'd':
-                    if tnum == len(self.PointList) - 1:
-                        tnum = 0
-                        tmp = input("현재 항목은 %s입니다.\n->"%self.PointList[tnum])
-                    else:
-                        tnum = tnum + 1
-                        tmp = input("현재 항목은 %s입니다.\n->" % self.PointList[tnum])
-
-                else:
-                    tmp = input("잘못된 입력입니다. 다시 입력해주세요 -> ")
-
-                tlist = self.menu_list.get(self.PointList[tnum])
-                tnum = 0
-
-                tmp = input("카테고리 선택이 완료되었습니다. 이제 아이템을 선택해주세요. 첫 항목은 %s입니다. 선택은 전과 동입니다.\n->"%tlist)
-                while not tmp == 'E':
-                    if tmp == 'a':
-                        if tnum == 0:
-                            tnum = len(tlist) - 1
-                            tmp = input("현재 항목은 %s입니다.\n->"%tlist[tnum])
-                        else:
-                            tnum = tnum - 1
-                            tmp = input("현재 항목은 %s입니다.\n->" % tlist[tnum])
-
-                    elif tmp == 'd':
-                        if tnum == len(tlist) - 1:
-                            tnum = 0
-                            tmp = input("현재 항목은 %s입니다.\n->"%tlist[tnum])
-                        else:
-                            tnum = tnum + 1
-                            tmp = input("현재 항목은 %s입니다.\n->" % tlist[tnum])
-                    else:
-                        tmp = input("잘못된 입력입니다. 다시 입력해주세요 -> ")
-        print("선택하신 항목은 %s입니다."%tlist[tnum])
-
-    def __del__(self):
-        print("리스트를 삭제하고 프로그램을 종료합니다.\n")
-        del(self.menu_list)
-
-Leizy = LeDict()
-Leizy.set_Dic()
-Leizy.show_Dic()
-Leizy.Do_Run()

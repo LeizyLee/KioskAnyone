be826fcf4839133ba2c3bbfa45df32bd1a03159e
LeizyLee <36654052+LeizyLee@users.noreply.github.com>
2018-10-15 12:52:19 +0900

    Update DB.py

--- DB.py
+++ DB.py
@@ -1,18 +1,32 @@
 import pymysql
 
-def get_item(_list, _key):
-    return [i[2:4] for i in _list if _key in i]
+class DBlist:
+    def __init__(self, _host='localhost', _user='root', _password='', _db='', _charset='utf8'):
+        self.conn = pymysql.connect(host=_host, user=_user, password=_password, db=_db, charset=_charset)
+        self.curs = self.conn.cursor()
+
+    def get_table(self):
+        sql = "select * from tb_cat"
+        self.curs.execute(sql)
+        return list(self.curs.fetchall())
+
+    def get_item(self, _key):
+        tmp_list = []
+        for i in self.get_table():
+            if _key in i[1]:
+                tmp_list.append(i[2:])
+        return tmp_list
+
+    def show_table(self):
+        print(list(self.get_table()))
 
 
-conn = pymysql.connect(host='right.jbnu.ac.kr', user='', password='',db='',charset='utf8')
+    def get_cat(self):
+        tmp = []
+        for i in self.get_table():
+            if not i[1][3:] in tmp:
+                tmp.append(i[1][3:])
+        return tmp
 
-curs = conn.cursor()
-sql = "select * from menulist"
-curs.execute(sql)
-
-rows = list(curs.fetchall())
-print(type(rows))
-
-conn.close()
-
-print(get_item(rows, "Coffee"))
+    def __del__(self):
+        self.conn.close()

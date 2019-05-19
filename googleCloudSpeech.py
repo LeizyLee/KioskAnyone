f42219915894bb3b39f53861a8e2768c2dce4972
LeizyLee <hwansang5@gmail.com>
2019-05-19 14:02:59 +0900

    정리
    정리

---
+++ googleCloudSpeech.py
@@ -0,0 +1,41 @@
+def run_quickstart():
+    # [START speech_quickstart]
+    import io
+
+    # Imports the Google Cloud client library
+    # [START migration_import]
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    # [END migration_import]
+
+    # Instantiates a client
+    # [START migration_client]
+    client = speech.SpeechClient()
+    # [END migration_client]
+
+    # The name of the audio file to transcribe
+    file_name = 'file.wav'
+
+    # Loads the audio into memory
+    with io.open(file_name, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = types.cloud_speech_pb2.RecognitionAudio(content=content)
+
+    config = types.cloud_speech_pb2.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='ko-KR')
+
+    # Detects speech in the audio file
+    response = client.recognize(config, audio)
+
+    text = ''
+    for result in response.results:
+        text = text + '{}'.format(result.alternatives[0].transcript)
+    return text
+    # [END speech_quickstart]
+
+
+if __name__ == '__main__':
+    run_quickstart()
